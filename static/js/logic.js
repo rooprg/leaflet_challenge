143e67f229c5ec07874ae54c24ad46f42d34e1c4
rooprg <richardroop.pmp@icloud.com>
2024-07-18 14:36:46 -0400

    Day 2 afternoon updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Create the tile layer that will be the background of the map
-let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 // Create an object to hold the streetmap layer
 let baseMaps = {
-    "Street Map": streetmap
+    "Street Map": streetMap
 };
 
 // Create an object to hold the earthquakes layer
@@ -14,13 +14,62 @@ let quakeMaps = {
 };
 
 // Create the map object
-let map = L.map(  , {
-    center: ,
-    zoom: ,
-    layers: [streetmap, quakeMaps]
+let map = L.map("map", {
+    center: [44.58, -103.46],
+    zoom: 10,
+    layers: [streetMap, quakeMaps]
 });
 
-// Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+// Create a layer control, and pass in baseMaps and overlayMaps. Add the layer control to the map.
 L.control.layers(baseMaps, quakeMaps, {
     collapsed: false
 }).addTo(map);
+
+// Create a function for the markers
+function createMarkers(response) {
+
+// Retrieve "place" property from response.data
+let places = response.features.properties.place;
+
+// Initialize an array to hold earthquakes
+let quakeMarkers = []
+
+// Loop through JSON
+for (let i = 0; i < response.length; i++)
+    let event = place[i];
+
+    // For each event, create a marker, plus bind a popup with name
+}
+
+
+
+
+
+
+
+// Create a function to determine the marker color based on earthquake depth
+function markerColor(depth) {
+    if (depth < 10) return "green";
+    else if (depth < 30) return "lightyellow";
+    else if (depth < 50) return "yellow";
+    else if (depth < 70) return "orange";
+    else if (depth < 90) return "red";
+    else return "pink";
+}
+
+  // Add circles to the map.
+  L.circle(, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: markerColor(features.geometry.coordinates[]),
+    // Adjust the radius.
+    radius: Math.sqrt() * 500
+  }).bindPopup(`<h1>${}</h1> <hr> <h3>Earthquake: ${}</h3>`).addTo(myMap);
+}
+
+
+
+
+// Set up the legend.
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {}
