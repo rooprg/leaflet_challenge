1ebe4d6ce1df97b783f70276d75d1b0e32b403c3
rooprg <richardroop.pmp@icloud.com>
2024-07-19 17:03:16 -0400

    Last Day 3 updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -68,3 +68,32 @@ function createCircles(response) {
 
 // Perform an API call to the API to get the earthquake information. Call createCircles when it completes.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(createCircles);
+
+
+// Configure the legend
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  let limits = ;
+  let colors = ;
+  let labels = [];
+
+  // Add the minimum and maximum.
+  let legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
+    "<div class=\"labels\">" +
+      "<div class=\"min\">" + limits[0] + "</div>" +
+      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    "</div>";
+
+  div.innerHTML = legendInfo;
+
+  limits.forEach(function(limit, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  });
+
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
+
+// Adding the legend to the map
+legend.addTo(myMap);
