133078ab3634a55a4526072c0f8ae38421075c48
rooprg <richardroop.pmp@icloud.com>
2024-07-19 12:11:52 -0400

    Day 3 afternoon updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,44 @@
+// Create a function for the markers
+function createCircles(response) {
+    let features = response.features;
+    
+    // Initialize an array to hold earthquakes
+    let quakeCircles = [];
+
+    features.forEach(feature => {
+        let id = feature.id;
+        let geometry = feature.geometry;
+        let coordinates = geometry.coordinates;
+        let depth = coordinates[2];
+        
+        function markerColor(depth) {
+            if (depth < 10) return "green";
+            else if (depth < 30) return "lightyellow";
+            else if (depth < 50) return "yellow";
+            else if (depth < 70) return "orange";
+            else if (depth < 90) return "red";
+            else return "pink";
+        }
+
+        // For each event, create a circle marker, plus bind a popup with name
+        let quakeCircle = L.circleMarker([coordinates[1], coordinates[0]], {
+            fillOpacity: 0.75,
+            color: "white",
+            fillColor: markerColor(depth),
+            radius: Math.sqrt(feature.properties.mag) * 500
+        }).bindPopup(`<h3>${id}</h3><h3>Coordinates: ${coordinates}</h3>`);
+
+        // Add markers to quakeMarkers array
+        quakeCircles.push(quakeCircle);
+    });
+
+    // Create a layer group that's made from the new array, and pass it to the createMap function.
+    createMap(L.layerGroup(quakeCircles));
+
+// Perform an API call to theAPI to get the station information. Call createCircles when it completes.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createCircles);
+
+
 function createMap(earthQuakes) {
 
 // Create the tile layer that will be the background of the map
@@ -28,54 +69,6 @@ L.control.layers(baseMaps, quakeMaps, {
 }).addTo(map);
 }
 
-// Create a function for the markers
-function createMarkers(response) {
-    let features = response.features;
-    
-    // Initialize an array to hold earthquakes
-    let quakeMarkers = [];
-
-    features.forEach(feature => {
-        let id = feature.id;
-        let geometry = feature.geometry;
-        let coordinates = geometry.coordinates;
-
-        // For each event, create a marker, plus bind a popup with name
-        let quakeMarker = L.marker([coordinates[1], coordinates[0]])
-            .bindPopup("<h3>" + id + "</h3><h3>Coordinates: " + coordinates + "</h3>");
-
-        // Add markers to quakeMarkers array
-        quakeMarkers.push(quakeMarker);
-    });
-
-    // Create a layer group that's made from the new array, and pass it to the createMap function.
-    createMap(L.layerGroup(quakeMarkers));
-}
-
-
-// Create a function to determine the marker color based on earthquake depth
-function markerColor(depth) {
-    if (depth < 10) return "green";
-    else if (depth < 30) return "lightyellow";
-    else if (depth < 50) return "yellow";
-    else if (depth < 70) return "orange";
-    else if (depth < 90) return "red";
-    else return "pink";
-}
-
-  // Add circles to the map.
-  L.circle(, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: markerColor(),
-    // Adjust the radius.
-    radius: Math.sqrt(features.properties.mag) * 500
-  }).bindPopup(`<h1>${}</h1> <hr> <h3>Earthquake: ${}</h3>`).addTo(myMap);
-}
-
-
-
-
 // Set up the legend.
-let legend = L.control({ position: "bottomright" });
-legend.onAdd = function() {}
+//et legend = L.control({ position: "bottomright" });
+//legend.onAdd = function() {}
